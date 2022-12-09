27eed7d77e9978418258595574c9bcad2c563987
WeiQi-0414 <weiqi0414@gmail.com>
2022-12-09 04:57:31 -0500

    add loginRouters.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const server = app.listen(port, ()=> console.log("Server listening on port " + p
 app.set("view engine", "pug");
 app.set("views","views");
 
+// Routes
+const loginRoute = require("./routes/loginRoutes");
+app.use("/login", loginRoute);
+
 app.get("/", middleware.requireLogin, (req, res, next)=>{
     var payload = {
         pageTitle:"Home"
