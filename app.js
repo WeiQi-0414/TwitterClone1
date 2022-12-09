48f762365609ed62a151dc54608138f35ef761c0
WeiQi-0414 <weiqi0414@gmail.com>
2022-12-09 04:15:52 -0500

    add layouts folder and main-layout.pug extend home

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.set("view engine", "pug");
 app.set("views","views");
 
 app.get("/",(req, res, next)=>{
-    res.status(200).render("home");
+    var payload = {
+        pageTitle:"Home"
+    }
+
+    res.status(200).render("home",payload);
 })
