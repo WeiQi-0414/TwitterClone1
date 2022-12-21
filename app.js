726c20af24f22e0b58689f8eb6b5bfa0df8b7635
WeiQi-0414 <weiqi0414@gmail.com>
2022-12-21 12:50:02 -0500

    install bcrypt,session hash password mogDB userNM

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require("./middleware");
 const path = require('path');
 const bodyParser = require("body-parser")
 const mongoose = require("./database");
+const session = require("express-session");
 
 
 const server = app.listen(port, ()=> console.log("Server listening on port " + port));
@@ -14,6 +15,11 @@ app.set("views","views");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+    secret:"big secret",
+    resave: true,
+    saveUninitialized:false
+}))
 
 // Routes
 const loginRoute = require("./routes/loginRoutes");
@@ -24,7 +30,8 @@ app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next)=>{
     var payload = {
-        pageTitle:"Home"
+        pageTitle:"Home",
+        userLoggedIn:req.session.user
     }
 
     res.status(200).render("home",payload);
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt");
+const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
 app.set("views","views");
+
 app.use(bodyParser.urlencoded({extended:false}));
 
 router.get("/", (req, res, next)=>{
@@ -12,15 +15,47 @@ router.get("/", (req, res, next)=>{
     res.status(200).render("register");
 })
 
-router.post("/", (req, res, next)=>{
+router.post("/", async (req, res, next)=>{
     var firstName = req.body.firstName.trim();
     var lastName = req.body.lastName.trim();
-    var userName = req.body.userName.trim();
+    var username = req.body.username.trim();
     var email = req.body.email.trim();
     var password = req.body.password;
     var payload = req.body;
 
-    if(firstName && lastName && userName && email && password){
+    if(firstName && lastName && username && email && password){
+        var user = await User.findOne({
+            $or:[{username:username},{email:email}]
+        })
+        .catch((error) => {
+            console.log(error);
+            payload.errorMessage = "Something went wrong.";
+            res.status(200).render("register", payload);
+        });
+        // checking if the user info is already in use
+        if(user == null){
+            //NO user found
+            var data = req.body;//collect the data that input
+            data.password = await bcrypt.hash(password,10);
+            //using the userschema to create database table 
+            User.create(data)
+            .then((user) =>{
+                req.session.user = user;
+                return res.redirect("/");
+            })
+        }
+        else{
+            //User found
+            if(email == user.email){
+                payload.errorMessage = "Email.already in use.";
+            }
+            else{
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("register", payload);
+        }
+
+
 
     }
     else{
